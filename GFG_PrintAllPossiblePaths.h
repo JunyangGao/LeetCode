28289584292d803df42d524c466096055a668fd2
junyang <junyang.gao@outlook.com>
2014-03-04 01:39:34 +0800

    fix bug

--- GFG_PrintAllPossiblePaths.h
+++ GFG_PrintAllPossiblePaths.h
@@ -13,54 +13,39 @@
  Solution: recursive.
  */
 
-#include <stddef.h>
 #include <iostream>
 using namespace std;
 
-class Solution {
+class SolutionPAPP {
 public:
     /*	mat:  Pointer to the starting of mXn matrix
-		i, j: Current position of the robot (For the first call use 0,0)
-		m, n: Dimentions of given the matrix
-		pi:   Next index to be filed in path array
-		*path[0..pi-1]: The path traversed by robot till now (Array to hold the
+        i, j: Current position of the robot (For the first call use 0,0)
+        m, n: Dimentions of given the matrix
+        pi:   Next index to be filed in path array
+        *path[0..pi-1]: The path traversed by robot till now (Array to hold the
                   path need to have space for at least m+n elements) */
-	void printAllPathsUtil(int *matrix, int i, int j, int m, int n, int *path, int pi)
-	{
-		if ((path == nullptr) || (matrix == nullptr))
-			return;
+    void printAllPathsUtil(int *matrix, int i, int j, int m, int n, int *path, int pi) {
+        if ((path == nullptr) || (matrix == nullptr))
+            return;
+        
+        path[pi] = *((matrix + i*n) + j);
 
-
-		if(i == m && j == n)
-		{
-			for (int k = 0; k < pi; ++k)
-				cout<<path[k]<<", ";
-
-			cout<<endl;
-			return;
-		}
-
-		path[pi] = *((matrix + i*n) + j);
-
-		if(i < m )
-
-			printAllPathsUtil(matrix, ++i, j, m, n, path, ++pi);
-		if(j < n)
-			printAllPathsUtil(matrix, ++i, j, m, n, path, ++pi);
+        if(i == m - 1 && j == n - 1)
+        {
+            for (int k = 0; k <= pi; ++k)
+                cout<<path[k]<<" ";
+            cout<<endl;
+            return;
+        }
+        if(i < m - 1)
+            printAllPathsUtil(matrix, i + 1, j, m, n, path, pi + 1);
+        if(j < n - 1)
+            printAllPathsUtil(matrix, i, j + 1, m, n, path, pi + 1);
     }
-	void printAllPaths(int *matrix, int m, int n)
-	{
-		
-		int *path = new int[m+n];
-		printAllPathsUtil( matrix, 0, 0, m, n, path, 0);
-		delete [] path;
-	}
+    void printAllPaths(int *matrix, int m, int n)
+    {
+        int *path = new int[m+n];
+        printAllPathsUtil( matrix, 0, 0, m, n, path, 0);
+        delete [] path;
+    }
 };
-// Driver program to test abve functions
-int main()
-{
-    int mat[2][3] = { {1, 2, 3}, {4, 5, 6} };
-	Solution s;
-    s.printAllPaths(*mat, 2, 3);
-    return 0;
-}
