1c73e8b3a2b4b68bd19eb8d76daf432fa5c83844
junyang <junyang.gao@outlook.com>
2014-03-04 02:24:36 +0800

    Count all possible paths from top left to bottom right of a mXn matrix

---
+++ GFG_CountAllPossiblePaths.h
@@ -0,0 +1,44 @@
+/*
+ Author:     Sparkle , junyang.gao@gmail.com
+ Date:       Mar 3, 2014
+ Update:     Mar 3, 2014
+ Problem:    Count all possible paths from top left to bottom right of a mXn matrix
+ Difficulty: Easy
+ Source:     http://www.geeksforgeeks.org/count-possible-paths-top-left-bottom-right-nxm-matrix/
+ Notes:
+ The problem is to count all the possible paths from top left to bottom right of a mXn matrix with the constraints that from each cell you can either move only to right or down
+
+ Solution: recursive and DP
+ */
+
+#include <iostream>
+#include <sstream>
+#include <map>
+using namespace std;
+
+
+class SolutionCAPP{
+public:
+    int numberOfPathsRecursive(int m, int n)
+    {
+        if(m == 1 || n == 1)
+            return 1;
+        return numberOfPathsRecursive(m - 1, n) + numberOfPathsRecursive(m, n - 1);
+    }
+
+    map<string, int> map;  
+    int numberOfPathsDP(int m, int n)
+    {
+        stringstream ss;
+        ss << (m*10 + n);
+        string key = ss.str();
+        
+        if(map.find(key) != map.end())
+            return map[key];
+
+        if(m == 1 || n == 1)
+            return 1;
+
+        return numberOfPathsDP(m - 1, n) + numberOfPathsDP(m, n - 1);
+    }
+};
